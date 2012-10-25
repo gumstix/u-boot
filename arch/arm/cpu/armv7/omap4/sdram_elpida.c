JamesAng/ub

omap: fix compile error in emif-common.c
ext_phy_ctrl_const_base is currently only defined if
CONFIG_SYS_EMIF_PRECALCULATED_TIMING_REGS is set.

If not set, the following error occurs:

In function `do_sdram_init':
| arch/arm/cpu/armv7/omap-common/emif-common.c:985: undefined reference to `ext_phy_ctrl_const_base'

Signed-off-by: Steve Sakoman <steve@sakoman.com>

arch/arm/cpu/armv7/omap4/sdram_elpida.c
@@ -46,6 +46,9 @@
  * - emif_get_device_timings()
  */
 
+/* Dummy registers for OMAP44xx */
+const u32 ext_phy_ctrl_const_base[EMIF_EXT_PHY_CTRL_CONST_REG];
+
 #ifdef CONFIG_SYS_EMIF_PRECALCULATED_TIMING_REGS
 
 static const struct emif_regs emif_regs_elpida_200_mhz_2cs = {
@@ -89,9 +92,6 @@ const struct emif_regs emif_regs_elpida_400_mhz_2cs = {
 	.emif_ddr_phy_ctlr_1_init	= 0x049ffff5,
 	.emif_ddr_phy_ctlr_1		= 0x049ff418
 };
-
-/* Dummy registers for OMAP44xx */
-const u32 ext_phy_ctrl_const_base[EMIF_EXT_PHY_CTRL_CONST_REG];
 
 const struct dmm_lisa_map_regs lisa_map_2G_x_1_x_2 = {
 	.dmm_lisa_map_0 = 0xFF020100,

gumstix/u-boot

arch/arm/cpu/armv7/omap4/sdram_elpida.c
@@ -46,6 +46,9 @@
  * - emif_get_device_timings()
  */
 
+/* Dummy registers for OMAP44xx */
+const u32 ddr3_ext_phy_ctrl_const_base[EMIF_EXT_PHY_CTRL_CONST_REG];
+
 #ifdef CONFIG_SYS_EMIF_PRECALCULATED_TIMING_REGS
 
 static const struct emif_regs emif_regs_elpida_200_mhz_2cs = {
@@ -89,9 +92,6 @@ const struct emif_regs emif_regs_elpida_400_mhz_2cs = {
 	.emif_ddr_phy_ctlr_1_init	= 0x049ffff5,
 	.emif_ddr_phy_ctlr_1		= 0x049ff418
 };
-
-/* Dummy registers for OMAP44xx */
-const u32 ddr3_ext_phy_ctrl_const_base[EMIF_EXT_PHY_CTRL_CONST_REG];
 
 const struct dmm_lisa_map_regs lisa_map_2G_x_1_x_2 = {
 	.dmm_lisa_map_0 = 0xFF020100,