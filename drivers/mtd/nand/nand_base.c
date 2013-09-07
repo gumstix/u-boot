2806cd7aafc1e3d338a68b230a6e48cc165ee26b
Ash Charles <ashcharles@gmail.com>
2013-09-06 19:08:36 -0700

    OMAP3: overo: Work around HW ECC error.
    This is a workaround to disable subpage reads when the
    ECC mode is not NAND_ECC_SOFT as discussed:
    http://u-boot.10912.n7.nabble.com/NAND-write-error-with-HW-ECC-on-OMAP3-td162420.html

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -3354,6 +3354,8 @@ int nand_scan_tail(struct mtd_info *mtd)
 	/* Large page NAND with SOFT_ECC should support subpage reads */
 	if ((chip->ecc.mode == NAND_ECC_SOFT) && (chip->page_shift > 9))
 		chip->options |= NAND_SUBPAGE_READ;
+	else
+		chip->options &= ~NAND_SUBPAGE_READ;
 
 	/* Fill in remaining MTD driver data */
 	mtd->type = MTD_NANDFLASH;
